1035272f1a8c0146a34513de5c55c77f6407dab3
Gregorio Caba <100645620+grichal@users.noreply.github.com>
2023-07-19 22:08:18 -0400

    se arreglo la redireccion del boton contactanos

--- src/firstSection/firstSection.jsx
+++ src/firstSection/firstSection.jsx
@@ -3,14 +3,19 @@ import imageRob from "../assets/image1.png"
 import './firsSection.css'
 
 const FirsSection =()=>{
+
+
+    const redirectP = (()=>{
+        window.location.href='#contactUs'
+    })
     return(<>
         <section className="firstsection" id="first">
 
             <div className="desc">
                 <h1 className="title">VISAS TO GO</h1>
                 <p>Es una oficina de consultas y tramites migratorios a estados unidos, de alta integridad y profesionalismo. su lisenciado, isaias grullon, fue sargento de la policia de la ciudad de nueva york, (NYDP), por 20 años. es graduado de john hay college of criminal justic, manhattan NY. Tambien esta certificado, en leyes y tramites migratorios por el US Inmigration institute, Orlando, FL,</p>
-                <button>CONTACTANOS</button>
-               <p className="DTCEN"> DEPOSITA TU CONFIANZA dddEN NOSOTROS</p>
+                <button onClick={redirectP}>CONTACTANOS</button>
+               <p className="DTCEN"> DEPOSITA TU CONFIANZA EN NOSOTROS</p>
             </div>
             <div className="image">
                 <img src={imageRob} alt="" />
