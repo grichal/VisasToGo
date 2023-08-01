3f9e82396b14f2dd9b7995887d8f0463a90a1cd5
Gregorio Caba <100645620+grichal@users.noreply.github.com>
2023-08-01 14:13:02 -0400

    se actualizaron cartas, y caracteristicas de la primera sesion

--- src/contactUs/contactUs.jsx
+++ src/contactUs/contactUs.jsx
@@ -3,6 +3,11 @@ import React from "react";
 import "./contactUs.css";
 
 const ContactUs = () => {
+
+  const redirectPhone = ()=>{
+    window.location.href = `tel:${8299593082}`
+  }
+
   return (
     <section className="contactUs" id="contactUs">
       <h1 className="contactTitle">CONTACTANOS</h1>
@@ -36,8 +41,8 @@ const ContactUs = () => {
               <h1>NUMERO DE </h1>
               <h1>OFICINA</h1>
             </div>
-            <div className="button" onClick={()=>window.location.href="https://wa.me/18297637928?text=Hola,%20quisiera%20saber%20mas%20acerca%20de%20los%20servicios%20de%20Visas%20to%20Go%20RD"}>
-              <button >HABLA VIA WHATSAPP</button>
+            <div className="button">
+              <button onClick={redirectPhone}>+1 (809) 365-2011</button>
             </div>
           </div>
         </div>
@@ -53,7 +58,7 @@ const ContactUs = () => {
               <h1>VISAIRA </h1>
               <h1>HERNANDEZ</h1>
             </div>
-            <div className="button" onClick={()=>window.location.href="https://wa.me/18297637928?text=Hola,%20quisiera%20saber%20mas%20acerca%20de%20los%20servicios%20de%20Visas%20to%20Go%20RD"}>
+            <div className="button" onClick={()=>window.location.href="https://wa.me/18493431019?text=Hola,%20quisiera%20saber%20mas%20acerca%20de%20los%20servicios%20de%20Visas%20to%20Go%20RD"}>
               <button>HABLA VIA WHATSAPP</button>
             </div>
           </div>
